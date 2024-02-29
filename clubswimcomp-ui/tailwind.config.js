757038de49283d3ff254ad44eb3d27b5148309d7
Pascal Koestler <pascal.koestler@outlook.de>
2024-02-29 20:35:50 +0100

    Change table to have no padding in first/last column

--- clubswimcomp-ui/tailwind.config.js
+++ clubswimcomp-ui/tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ["./src/**/*.rs"],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "corporate", "business"],
+    themes: [
+      {
+        corporate: {
+          ...require("daisyui/src/theming/themes")["corporate"],
+          "primary-content": "FFFFFF"
+        }
+      }
+    ],
   }
 }
 
